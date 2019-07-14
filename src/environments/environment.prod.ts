c89495c9fa6997418b6eac3ec182fdee7d9a66ea
Michael Solati <mkslt04@gmail.com>
2019-07-14 16:14:38 -0600

    feat: add `@angular/fire` and `@angular/material`

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDn42FnOboOZoqqMj7Y-4hYRt8ZN8MpJFk',
+    authDomain: 'withgdg.firebaseapp.com',
+    databaseURL: 'https://withgdg.firebaseio.com',
+    projectId: 'withgdg',
+    storageBucket: 'withgdg.appspot.com',
+    messagingSenderId: '727740528734',
+    appId: '1:727740528734:web:2235e7049f02f249'
+  }
 };
