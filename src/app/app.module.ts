a5cad0c5f41a2fb99e0f9fa6116068448630187e
Michael Solati <mkslt04@gmail.com>
2019-07-16 16:30:05 -0700

    feat(NavbarComponent): add navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { LayoutModule } from '@angular/cdk/layout';
 import { AgmCoreModule } from '@agm/core';
 
 import { CoreModule } from '@core';
 import { environment } from '@environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule.forRoot(),
+    MatToolbarModule,
+    MatSidenavModule,
+    LayoutModule,
     AgmCoreModule.forRoot({
       apiKey: environment.firebase.apiKey
     })
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'gdg-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  private _isHandset$: Observable<boolean> = this._breakpointObserver.observe(Breakpoints.Small)
+    .pipe(map(result => !(result.matches)));
+
+  constructor(private _breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit() {
+  }
+
+  get isHandset$(): Observable<boolean> {
+    return this._isHandset$;
+  }
+}
