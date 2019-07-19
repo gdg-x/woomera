a651c757056e83ce466704d722ce9d54eab0583b
Michael Solati <mkslt04@gmail.com>
2019-07-19 14:03:56 -0400

    fix: change icons, make map less greedy, and make cross-origin links "safe"

--- src/app/chapter/chapter.module.ts
+++ src/app/chapter/chapter.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 import { SharedModule } from '@shared';
@@ -19,6 +20,7 @@ import { ChapterComponent } from './chapter.component';
     SharedModule,
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
     MatListModule
   ]
 })
