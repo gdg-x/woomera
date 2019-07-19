44b10c9dbf4c3457df511405ed8282797f249130
Michael Solati <mkslt04@gmail.com>
2019-07-19 15:44:57 -0400

    test(BannerImagePipe): add dependencies to test

--- src/app/shared/pipes/banner-image.pipe.spec.ts
+++ src/app/shared/pipes/banner-image.pipe.spec.ts
@@ -1,8 +1,20 @@
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
+import { inject, TestBed } from '@angular/core/testing';
+
 import { BannerImagePipe } from './banner-image.pipe';
 
 describe('BannerImagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new BannerImagePipe();
-    expect(pipe).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [BrowserModule]
+    });
   });
+
+  it('create an instance', inject(
+    [DomSanitizer],
+    (domSanitizer: DomSanitizer) => {
+      const pipe = new BannerImagePipe(domSanitizer);
+      expect(pipe).toBeTruthy();
+    }
+  ));
 });
