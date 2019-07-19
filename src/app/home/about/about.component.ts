7d089dc94fe8faf31641874a108e711a25fb85e4
Michael Solati <mkslt04@gmail.com>
2019-07-19 11:41:00 -0400

    feat(AboutComponent): add component giving info about what GDGs do

---
+++ src/app/home/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gdg-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AgmCoreModule } from '@agm/core';
@@ -27,6 +28,7 @@ import { AboutComponent } from './about/about.component';
     HomeRoutingModule,
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
     MatListModule,
     MatProgressSpinnerModule,
     AgmCoreModule
