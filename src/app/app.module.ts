b950e282898da0c2cbc45f691e9c2d81b64c6566
Michael Solati <mkslt04@gmail.com>
2019-07-24 16:07:34 -0700

    feat(NavbarComponent): add sidenav and link to `/chaper` route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { LayoutModule } from '@angular/cdk/layout';
@@ -25,8 +28,11 @@ import { NavbarComponent } from './navbar/navbar.component';
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     CoreModule.forRoot(),
-    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
     MatSidenavModule,
+    MatToolbarModule,
     LayoutModule,
     AgmCoreModule.forRoot({
       apiKey: environment.firebase.apiKey
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, first } from 'rxjs/operators';
+
+type Path = {
+  name: string;
+  link: string[];
+  hideInNav?: boolean;
+};
 
 @Component({
   selector: 'gdg-navbar',
@@ -9,8 +16,13 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @ViewChild('drawer', { static: true }) drawer: MatSidenav;
   private _isHandset$: Observable<boolean> = this._breakpointObserver.observe(['(max-width: 599px)'])
     .pipe(map(result => result.matches));
+  private _paths: Path[] = [
+    { name: 'Home', link: ['/'], hideInNav: true },
+    { name: 'Chapters', link: ['/', 'chapter'], hideInNav: false }
+  ];
 
   constructor(private _breakpointObserver: BreakpointObserver) { }
 
@@ -19,5 +31,21 @@ export class NavbarComponent implements OnInit {
 
   get isHandset$(): Observable<boolean> {
     return this._isHandset$;
+  }
+
+  get paths(): Path[] {
+    return this._paths;
+  }
+
+  @HostListener('window:resize')
+  public resize(): void {
+    if (typeof window !== 'undefined' && window.innerWidth >= 601 && this.drawer.opened) {
+      this.drawer.close();
+    }
+  }
+
+  @HostListener('swiperight')
+  public swiperight(): void {
+    this._isHandset$.pipe(first()).subscribe((isHandset) => isHandset ? this.drawer.open() : null);
   }
 }
